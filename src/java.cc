4d2d9650ad3840d5e62916a65fc398d45fb29da3
Valery Tolstov <me@vtolstov.org>
2016-05-14 14:46:20 +0300

    Custom class path for java

--- src/java.cc
+++ src/java.cc
@@ -2,8 +2,13 @@
 #include <class/java_class.h>
 #include <jvm/jvm.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    std::string classPath = argv[1];
+
+    size_t found = classPath.find_last_of('.');
+    std::string className = classPath.substr(0, found);
+
     Class *cls = ClassLoader::loadClass("Sample");
     Method *mainMethod =
             cls->getMethod("main", "([Ljava/lang/String;)V");
